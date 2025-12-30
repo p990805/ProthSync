8f30efa71de4be8bf70b19a261609f685c6fab50
박주찬 <p990805@naver.com>
2025-12-31 02:24:07 +0900

    feat: 프로젝트 생성

---
+++ prothsync/src/main/java/com/prothsync/prothsync/ProthSyncApplication.java
@@ -0,0 +1,13 @@
+package com.prothsync.prothsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProthSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProthSyncApplication.class, args);
+	}
+
+}
