12a4ef1fbb732d138e43e1151dbee1f006de795c
박주찬 <p990805@naver.com>
2026-01-31 23:52:51 +0900

    feat: 임시 게시글 엔티티 개발 중

---
+++ prothsync/src/main/java/com/prothsync/prothsync/entity/post/Post.java
@@ -0,0 +1,54 @@
+package com.prothsync.prothsync.entity.post;
+
+import com.prothsync.prothsync.common.BaseEntity;
+import com.prothsync.prothsync.entity.user.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "posts")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseEntity {
+
+    private static final int MAX_CONTENT_LENGTH = 2000;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(length = 2000)
+    private String content;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PostVisibility visibility;
+
+    @Column(nullable = false)
+    private int likeCount = 0;
+
+    @Column(nullable = false)
+    private int commentCount = 0;
+
+    @Column(nullable = false)
+    private int viewCount = 0;
+
+
+
+}
