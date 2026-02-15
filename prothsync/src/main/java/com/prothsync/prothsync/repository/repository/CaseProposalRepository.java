9cb29d577855fe85064bbac7277d2bbf4e4aa95a
박주찬 <p990805@naver.com>
2026-02-15 21:36:36 +0900

    feat: 외주 작업 repository 개발

---
+++ prothsync/src/main/java/com/prothsync/prothsync/repository/repository/CaseProposalRepository.java
@@ -0,0 +1,29 @@
+package com.prothsync.prothsync.repository.repository;
+
+import com.prothsync.prothsync.entity.caserequest.CaseProposal;
+import com.prothsync.prothsync.entity.caserequest.ProposalStatus;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface CaseProposalRepository {
+
+    CaseProposal save(CaseProposal proposal);
+
+    Optional<CaseProposal> findById(Long proposalId);
+
+    void delete(CaseProposal proposal);
+
+    Page<CaseProposal> findAllByCaseRequestId(Long caseRequestId, Pageable pageable);
+
+    Page<CaseProposal> findAllByProposerId(Long proposerId, Pageable pageable);
+
+    boolean existsByCaseRequestIdAndProposerId(Long caseRequestId, Long proposerId);
+
+    List<CaseProposal> findAllByCaseRequestIdAndStatus(Long caseRequestId, ProposalStatus status);
+
+    long countByCaseRequestId(Long caseRequestId);
+
+    void deleteAllByCaseRequestId(Long caseRequestId);
+}
